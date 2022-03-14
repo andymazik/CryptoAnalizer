02f3b0d9d8563db0133e682b2fd1749920a9e7b8
Andrey Mazikov <andymazik@gmail.com>
2022-03-14 16:34:15 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Mazikov project
---
+++ src/main/java/ru/javarush/mazikov/cryptoanalizer/Runner.java
@@ -0,0 +1,7 @@
+package ru.javarush.mazikov.cryptoanalizer;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Hello maven");
+    }
+}
